1f996f072f9d0c95ca11bb414fb4c1927b4dd9d3
Patrick Rodriguez <patr84@gmail.com>
2011-09-20 02:59:10 -0700

    Larger random blue grid.

--- src/clj_genart/chp2.clj
+++ src/clj_genart/chp2.clj
@@ -2,18 +2,19 @@
   (:use [rosado.processing]
         [rosado.processing.applet]))
 
-(def *width* 500)
-(def *height* 300)
+(def *width* 400)
+(def *height* 700)
 
 (defn draw []
   "Evaluate to draw next frame."
+  (framerate 2)
   (background-float 230 230 230)
-  (stroke-float 130 0 0)
+  (stroke-float 0 0 0)
   (stroke-weight 1)
   (let [padding 20]
     (doseq [x (range (/ padding 2) (- *width* padding) padding)
             y (range (/ padding 2) (- *height* padding) padding)]
-      (stroke-weight (rand-int 3))
+      (fill-float 0 0 255 (rand-int 100))
       (rect x y 15 15))))
 
 
