b2dd4846e0ab50c484afc12f3bbfbcbdca35296e
Patrick Rodriguez <patr84@gmail.com>
2011-09-20 02:53:40 -0700

    Neat grid pattern!

--- src/clj_genart/chp2.clj
+++ src/clj_genart/chp2.clj
@@ -2,17 +2,20 @@
   (:use [rosado.processing]
         [rosado.processing.applet]))
 
+(def *width* 500)
+(def *height* 300)
+
 (defn draw []
   "Evaluate to draw next frame."
   (background-float 230 230 230)
   (stroke-float 130 0 0)
-  (stroke-weight 4)
-  (let [cx (/ (width) 2)
-        cy (/ (height) 2)]
-    (line (- cx 70) (- cy 70) (+ cx 70) (+ cy 70))
-    (line (- cx 70) (+ cy 70) (+ cx 70) (- cy 70))
-    (fill 255 150)
-    (ellipse cx cy 50 50)))
+  (stroke-weight 1)
+  (let [padding 20]
+    (doseq [x (range (/ padding 2) (- *width* padding) padding)
+            y (range (/ padding 2) (- *height* padding) padding)]
+      (stroke-weight (rand-int 3))
+      (rect x y 15 15))))
+
 
 (defn setup []
   "Runs once."
@@ -20,7 +23,7 @@
   (no-loop))
 
 (defapplet app :title "Chapter 2"
-  :setup setup :draw draw :size [500 300])
+  :setup setup :draw draw :size [*width* *height*])
 
 (run app)
 
