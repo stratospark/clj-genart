64f394ffd2e7127fc0eab8501d11d3744b7d1d31
Patrick Rodriguez <patr84@gmail.com>
2011-09-20 03:09:18 -0700

    Fig 2.7

--- src/clj_genart/chp2.clj
+++ src/clj_genart/chp2.clj
@@ -2,26 +2,29 @@
   (:use [rosado.processing]
         [rosado.processing.applet]))
 
-(def *width* 400)
-(def *height* 700)
+(def *width* 500)
+(def *height* 300)
+(def diam (atom 10))
 
 (defn draw []
   "Evaluate to draw next frame."
-  (framerate 2)
-  (background-float 230 230 230)
-  (stroke-float 0 0 0)
-  (stroke-weight 1)
-  (let [padding 20]
-    (doseq [x (range (/ padding 2) (- *width* padding) padding)
-            y (range (/ padding 2) (- *height* padding) padding)]
-      (fill-float 0 0 255 (rand-int 100))
-      (rect x y 15 15))))
-
+  (framerate 24)
+  (smooth)
+  (background-float 180)
+  (let [cx (/ (width) 2)
+        cy (/ (height) 2)]
+    (stroke-float 0)
+    (stroke-weight 5)
+    (fill-float 255 50)
+    (ellipse cx cy @diam @diam)
+    (swap! diam + 10)
+    (if (> @diam 400)
+      (reset! diam 10))))
 
 (defn setup []
   "Runs once."
-  (smooth)
-  (no-loop))
+  (smooth))
+
 
 (defapplet app :title "Chapter 2"
   :setup setup :draw draw :size [*width* *height*])
